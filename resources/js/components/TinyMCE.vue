08f5b8f24abcb07690c68f5dbdeeec97ea631c0e
codershisho <codershisho@gmail.com>
2023-08-26 21:35:37 +0900

    不要な属性を削除

--- resources/js/components/TinyMCE.vue
+++ resources/js/components/TinyMCE.vue
@@ -7,19 +7,13 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  disable: {
-    type: Boolean,
-    default: false,
-  },
 })
 
 // エディタのmodel
 const editor = ref<string>(props.html)
-const editorDisable = ref<boolean>(props.disable)
 let editorToolbar = ref<string>('')
-editorToolbar.value = editorDisable.value
-  ? 'false'
-  : 'saveButton bold italic underline strikethrough h1 h2 h3 h4 numlist bullist checklist fontsize forecolor backcolor code hr indent outdent lineheight table'
+editorToolbar.value =
+  'bold underline strikethrough h1 h2 h3 h4 numlist bullist fontsize forecolor backcolor code hr indent outdent lineheight'
 
 type Emits = { (e: 'update', value: object): void }
 const emit = defineEmits<Emits>()
@@ -43,28 +37,21 @@ const editorInitObj = reactive({
         editor.execCommand('Strikethrough', true, true)
       }
     )
-    editor.ui.registry.addButton('saveButton', {
-      text: 'Save',
-      icon: 'save',
-      tooltip: '保存',
-      onAction: () => save(),
-    })
   },
   // リストの際にtabでインデント
   lists_indent_on_tab: true,
   // メニューを非表示
   menubar: false,
-  placeholder: 'ここに記載（markdown使用可）...',
+  placeholder: 'ここに記載...',
 })
 </script>
 
 <template>
   <Editor
     api-key="uxuv5mcna6rpk2blhue26reiad1yc455ujsix9mznz3jyroy"
-    plugins="code lists table"
+    plugins="code lists"
     :toolbar="editorToolbar"
     :init="editorInitObj"
     v-model="editor"
-    :disabled="editorDisable"
   />
 </template>
