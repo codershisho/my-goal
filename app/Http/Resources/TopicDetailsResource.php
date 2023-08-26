f5540c112648c5eeed73a4bf6632899ddcb2f033
codershisho <codershisho@gmail.com>
2023-08-26 16:27:10 +0900

    修正

--- app/Http/Resources/TopicDetailsResource.php
+++ app/Http/Resources/TopicDetailsResource.php
@@ -12,8 +12,8 @@ class TopicDetailsResource extends JsonResource
     public function toArray($request)
     {
         return [
-            "topic_id" => $this->id,
-            "topic_name" => $this->name,
+            "topic_detail_id" => $this->id,
+            "topic_detail_name" => $this->name,
         ];
     }
 }
